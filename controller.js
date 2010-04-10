ef7afd8ef55d959c29aeabe09b36f01036223134
Micha Niskin <asdf@ubergibson.com>
2010-04-10 21:41:29 +0000

    events

--- controller.js
+++ controller.js
@@ -1,9 +1,39 @@
+$.golf.defaultRoute = "/connect/";
+
+$(document).bind("imap_state", function(event, imap) {
+  console.log("state change: "+imap.state);
+  if (imap.state == 0)
+    $.golf.location("/connect/");
+  if (imap.state == 1)
+    $.golf.location("/login/");
+});
+
+var Imap = $.require("imap").Imap;
+var imap;
+
 $.golf.controller = [
 
-  { route: ".*",
+  { route: "/connect/",
     action: function(container, params) {
-      container.empty().append(new Component.IMAP());
+      container.empty().append("<h3>connecting...</h3>");
+      imap = new Imap("/mailproxy/", "ubergibson.com", 143);
+      window.imap = imap; // debugging
+    }
+  },
+
+  { route: "/login/",
+    action: function(container, params) {
+      if (!imap || imap.state != 0)
+        return $.golf.location("/connect/");
+      container.empty().append(new Component.IMAP(imap));
+    }
+  },
+
+  { route: "/.*/",
+    action: function(container, params) {
+      container.empty().append(new Component.Mailbox(imap));
     }
   }
 
 ];
+
--- plugins/imap.js
+++ plugins/imap.js
@@ -1,31 +1,106 @@
 #!coffee -p
 
-token: ((s, pat, hlr) ->
-  if (s.match(pat))
-    return hlr(s.replace(pat, ""))
+token: ((s, pat) ->
+  if ( s != undefined && (match: s.text.match(pat)) )
+    s.text: s.text.replace(pat, "")
+    s._match: match
+    return s
   else
     return undefined
 )
 
+parse_status_resp: ((resp) ->
+  pat: /^(OK|NO|BAD|PREAUTH|BYE)( \[(ALERT|BADCHARSET|CAPABILITY|PARSE|PERMANENTFLAGS|READ-ONLY|READ-WRITE|TRYCREATE|UIDNEXT|UIDVALIDITY|UNSEEN)\])?( ([^\r\n]+))?\r\n/i
+  if ( token(resp, pat) == undefined ) then return undefined
+
+  resp.status: resp._match[1]
+  resp.code: resp._match[3]
+  resp.args: resp._match[5]
+  $(document).trigger("imap_status", [resp])
+  return resp
+)
+
+parse_list_resp: ((resp) ->
+  pat: /^LIST \(([^\)]+)*\) "(.)" "([^\r\n]+)"\r\n/i
+  if ( token(resp, pat) == undefined ) then return undefined
+
+  resp.list: true
+  resp.attr: resp._match[1].split(" ")
+  resp.separator: resp._match[2]
+  resp.name: resp._match[3]
+  $(document).trigger("imap_list", [resp])
+  return resp
+)
+
+parse_untagged: ((resp) ->
+  if (token(resp, /^\* /) == undefined) then return undefined
+  resp.type: "untagged"
+  return parse_status_resp(resp) || 
+    parse_list_resp(resp)
+)
+
+parse_continuation: ((resp) ->
+  if (token(resp, /^\+ /) == undefined) then return undefined
+  resp.type: "continuation"
+  console.log(resp)
+  return resp
+)
+
+parse_tagged: ((resp) ->
+  if (token(resp, /^([A-Z0-9]+) /) == undefined) then return undefined
+  resp.id: resp._match[1]
+  resp.type: "tagged"
+  return parse_status_resp(resp)
+)
+
+parse_one: ((resp) ->
+  return parse_untagged(resp) ||
+    parse_continuation(resp) ||
+    parse_tagged(resp) ||
+    console.log("OH SHIT: '"+resp.text+"'")
+)
+
 parse: ((resp) ->
-  return token(resp, /^\* /, parse_untagged) ||
-    token(resp, /^\+ /, parse_continuation) ||
-    token(resp, /^[A-Z]+[0-9]+ /, parse_tagged)
+  while (resp? && resp.text? && resp.text.length > 0)
+    resp: parse_one( { text: resp.text } )
 )
 
 Imap: ((url, host, port) ->
   @url:   url
   @host:  host
   @port:  port
+
+  # 0 => not connected
+  # 1 => connected
+  # 2 => authenticated
+  # 3 => selected
+  @state: 0
   
-  @buf:   ""
-  @start: 0
+  @queue: []
 
   connect: ( ->
     $.ajax({
       url: url,
-      data: { rhost:"ubergibson.com", rport:143 }
+      data: { rhost:host, rport:port }
     })
+  )
+
+  $(document).bind("imap_status", (event, resp) => 
+    if (resp.type == "tagged") then return
+
+    if (@state == 0 && resp.status == "OK")
+      @setState(1)
+      console.log(resp.args)
+    else if (@state == 0 && resp.status == "PREAUTH")
+      @setState(2)
+      console.log(resp.args)
+    else if (@state > 0 && resp.status == "BYE")
+      @setState(0)
+      console.log(resp.args)
+  )
+
+  $(document).bind("imap_state", (event, i) ->
+    console.log(">>> "+i.state)
   )
 
   $(document).ajaxComplete(
@@ -38,18 +113,25 @@ Imap: ((url, host, port) ->
           console.log(xhr.responseText)
           @start: 0
           return connect()
-        else if (@start++ == 0)
-          console.log(xhr.responseText)
         else
-          @buf += xhr.responseText
+          console.log(xhr.responseText)
+          parse({ text: xhr.responseText })
         $.get(set_url)
       )
     )
   )
 
+  @setState(0)
   connect()
 
   setInterval((ping: ( => @noop())), 150000)
+
+  return this
+)
+
+Imap.prototype.setState: ((state) ->
+  @state: state
+  $(document).trigger("imap_state", [this])
 
   return this
 )
@@ -69,43 +151,29 @@ Imap.prototype.next: ((id, fmt) ->
   )
 )(0, "XXXX0000")
 
-parseUntagged: ((untagged) ->
-  p_status: /^\* (OK|NO|BAD|PREAUTH|BYE)( \[([^\]]+)\])?( (.*))?\r\n/i
-)
-
-Imap.prototype.send: ((cmd, success, failure) ->
+Imap.prototype.send: ((cmd, success, failure, handlers) ->
   id: @next()
-  verb: cmd.replace(/ .*$/, "")
-  pattern: 
-    new RegExp("(^|^(.|\r|\n)*\r\n)("+id+") (OK|NO|BAD)( (\\[[^\\]]+\\]))?( (.*))?\r\n$")
 
   cmd: id + " " + cmd + "\r\n"
   console.log(cmd)
   $.post(this.url, { command: cmd })
 
-  setTimeout((checkResponse: ( =>
-    match:  @buf.match(pattern)
-    resp:   {}
+  if (handlers?)
+    for hlr of handlers
+      $(document).bind(hlr+"."+id, (event, resp) -> handlers[hlr](resp))
 
-    if (match)
-      console.log(@buf)
-      @buf: ""
+  $(document).bind("imap_status."+id, (event, resp) ->
+    if (resp.type != "tagged" || resp.id != id) then return
 
-      parseUntagged(String(match[1]))
+    console.log("send: id="+resp.id+", status="+resp.status)
 
-      resp: {
-        untagged:   match[1]
-        id:         match[3]
-        status:     match[4]
-        code:       match[6]
-        code_args:  match[8]
-      }
+    if (resp.status == "OK" && success?)
+      success(resp)
+    else if (failure?)
+      failure(resp)
 
-      if (success)
-        (if (resp.status=="OK" || !(failure?)) then success else failure)(resp)
-    else
-      setTimeout(checkResponse, 100)
-  )), 100)
+    $(document).unbind("."+id)
+  )
    
   return this
 )
@@ -116,7 +184,12 @@ Imap.prototype.noop: ((success, failure) ->
 )
 
 Imap.prototype.login: ((user, pass, success, failure) ->
-  @send("LOGIN \""+user+"\" \""+pass+"\"", success, failure)
+  @send("LOGIN \""+user+"\" \""+pass+"\"", ((resp) =>
+    @setState(2)
+    success(resp)
+  ), ((resp) ->
+    failure(resp.args)
+  ))
   return this
 )
 
@@ -126,19 +199,15 @@ Imap.prototype.select: ((mailbox, success, failure) ->
 )
 
 Imap.prototype.list: ((success, failure) ->
-  fin: ((resp) ->
-    pat: /(^|\r\n)\* LIST( \((.*)\))? "(.*)" "(.*)"(?=$|\r\n)/g
-    ret: (while ( (match: pat.exec(resp.untagged)) )
-      { 
-        flags: match[3].split(" ")
-        path_separator: match[4]
-        name: match[5]
-      }
-    )
-    if (success)
-      (if (resp.status=="OK" || !(failure?)) then success else failure)(ret)
-  )
-  @send('LIST "" %', fin)
+  boxes: []
+
+  handlers: {
+    "imap_list": ((resp) -> boxes.push(resp))
+  }
+
+  _success: ( -> success(boxes))
+
+  @send('LIST "" %', _success, failure, handlers)
   return this
 )
 
