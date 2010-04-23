5897d53925bffb93b4adfae903e87f988baf071f
Micha Niskin <asdf@ubergibson.com>
2010-04-23 12:12:52 -0400

    parser test script

---
+++ test_parser.js
@@ -0,0 +1,20 @@
+/*
+ * First run jison on plugins/imap/parser.jison to generate ./parser.js.
+ * Then run this script like `narwhal test_parser.js`.
+ */
+
+var parser = require("./parser").parser;
+
+parser.yy = { 
+  out: {},
+  trigger: function() { 
+    print(JSON.stringify(parser.yy.out)); 
+    parser.yy.out = {};
+  } 
+};
+
+var cmd = "+ 01WAsP+/dc==\r\n";
+cmd    += "* BYE [BADCHARSET ({4}\r\n";
+cmd    += "poiu asdf \"this is {2}\r\n a test\" zxcv)] Dis{2}\r\n";
+
+parser.parse(cmd);
